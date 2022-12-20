3a76f592841dda52bfd7ff5420e5a1d0eaed10d1
Carefree-happy <2507057523@qq.com>
2022-12-20 21:06:42 +0800

    feat: 区分环境

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
+console.log('process.env.NODE_ENV=', process.env.NODE_ENV) // 打印环境变量
+
+const config = {
     mode: "development", 
     entry: "./src/index.js", 
     output: {
@@ -26,3 +28,9 @@ module.exports = {
         new CleanWebpackPlugin(), // 引入清包插件
     ]
 };
+
+module.exports = (env, argv) => {
+    console.log('argv.mode=',argv.mode) // 打印 mode(模式) 值
+    // 这里可以通过不同的模式修改 config 配置
+    return config;
+}
